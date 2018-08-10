c4f0af76dfdfb327a5a6cfb1401ef72f80f3de6e
streeetspirit <tracerspirit@gmail.com>
2018-08-09 22:58:26 -0700

    added player controller

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
-const width = 101*5;
+const tile = 101;
+const tile_y = 83;
+const width = tile*5;
 const height = 83*7;
+
 // Enemies our player must avoid
 class Enemy {
-  constructor (speed, y) {
+  constructor (speed, y, x = -tile) {
     // The image/sprite for our enemies, moving speed, and starting position
     this.sprite = 'images/enemy-bug.png';
-    this.size = 101;
-    this.x = -this.size;
+    this.size = tile;
+    this.x = x;
     this.y = y;
     this.speed = speed;
 
@@ -29,10 +32,12 @@ class Enemy {
   }
 
   //enemies check if any collision with a player happened
-  checkCollision(player) {
-    if (this.y===player.y && !(this.x > player.x + player.size ||  this.x + this.size < player.x )) {
+  static checkCollision(enemies,x,y) {
+    for (enemy of enemies) {
+      if (enemy.y===y && !(enemy.x > x + tile ||  enemy.x + tile < x )) {
       detectedCollision();
     }
+  }
   }
 
 };
@@ -51,8 +56,22 @@ class Player {
 
   }
 
+// check if a player drifted off the screen
+// if a player reached the top water layer - add points and reset the player
   update() {
-
+    if (this.x > width-tile) {
+      this.x = width-tile;
+    }
+    if (this.x < 0) {
+      this.x = 0;
+    }
+    if (this.y > height-171) {
+      this.y = height-171;
+    }
+    if (this.y < tile_y) {
+      win();
+      reset();
+    }
   }
 
   // Draw on the screen
@@ -61,29 +80,48 @@ class Player {
   }
 
   handleInput(key) {
+    switch (key) {
+      case 'left': { this.x -= tile; console.log(key);};
+      break;
+      case 'up': this.y -= tile_y;
+      break;
+      case 'right': this.x += tile;
+      break;
+      case 'down': this.y += tile_y;
+      break;
 
+    }
   }
 
 };
 
+function detectCollision() {
+
+}
+
+function win() {
+
+}
+
+function reset() {
+
+}
+/* param level - sets up difficulty (amount of enemies spawned)
+ * then create an array of enemies
+ * enemies get random position between 3 lanes and a random speed between 200 and 400 for level 3 (it's proportional to difficulty)
+ */
 let level = 3;
 var allEnemies = [];
-
-for (let i = 0; i < level; i++) {
-  const enemy = new Enemy(Math.floor((Math.random() * 200) + 200), Math.floor((Math.random() * 3) + 1)*83-20);
-  console.log(enemy);
+for (let i = 0; i < level+1; i++) {
+  const enemy = new Enemy(Math.floor((Math.random() * 200) + 100 * (level-1)), Math.floor((Math.random() * 3) + 1)*83-20, Math.floor(-tile*(Math.random()*3 + 1)));
   allEnemies.push(enemy);
 }
-console.log(allEnemies);
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // if enemy has reached the end of the screen remove it from the array
 // Place the player object in a variable called player
 var player = new Player('images/char-boy.png');
-
-function detectCollision() {
-
-}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -94,6 +132,6 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
+    console.log(e.keyCode);
     player.handleInput(allowedKeys[e.keyCode]);
 });
